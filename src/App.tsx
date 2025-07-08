53028014b95c02190b45c83c7449627537fead05
Wiley Evans <thinknoise@users.noreply.github.com>
2025-07-08 15:59:46 -0700

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 // src/App.tsx
 import AudioBlobularPlayer from "./components/AudioBlobularPlayer";
 import AudioPondMenu from "./components/AudioBlobularPlayer/AudioPondMenu";
-import { BlobPanelAudioProvider } from "./hooks/useBlobPanelAudio";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-blobular">
-      <BlobPanelAudioProvider>
-        <h1>Blobular</h1>
-        <AudioBlobularPlayer />
-        <AudioPondMenu />
-      </BlobPanelAudioProvider>
+      <h1>Blobular</h1>
+      <AudioBlobularPlayer />
+      <AudioPondMenu />
     </div>
   );
 }
